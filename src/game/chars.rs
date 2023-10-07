faa7e7fa5c023165d3d770171d4af12f4afbf3df
minstrike520 <minstrike520@gmail.com>
2023-10-07 09:32:25 +0800

    (chars) chore: change multiplierperc to f32

--- src/game/chars.rs
+++ src/game/chars.rs
@@ -1,14 +1,14 @@
 pub struct Attribute {
     pub base: i32,
-    multiplier_percentage: f32,
+    multiplier_percentage: i32,
     addend: i32,
 }
 impl Attribute {
     fn new(base: i32) -> Self {
-        Self { base, multiplier_percentage: 0.0, addend: 0 }
+        Self { base, multiplier_percentage: 0, addend: 0 }
     }
     pub fn get(&self) -> i32 {
-        (self.base as f32 * (1.0 + self.multiplier_percentage) + self.addend as f32 + 0.5).floor() as i32
+        (self.base as f32 * (1.0 + self.multiplier_percentage as f32 / 100.0) + self.addend as f32 + 0.5).floor() as i32
     }
 }
 pub struct CharacterAttributes {
