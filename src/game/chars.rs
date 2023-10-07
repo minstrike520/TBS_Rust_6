8f1d06e69fe0baec6546a45ffd97ebf132305b59
minstrike520 <minstrike520@gmail.com>
2023-10-07 09:40:33 +0800

    (chars) chore: use tune in attr calculate

--- src/game/chars.rs
+++ src/game/chars.rs
@@ -1,3 +1,4 @@
+use crate::game::tools;
 pub struct Attribute {
     pub base: i32,
     multiplier_percentage: i32,
@@ -8,7 +9,7 @@ impl Attribute {
         Self { base, multiplier_percentage: 0, addend: 0 }
     }
     pub fn get(&self) -> i32 {
-        (self.base as f32 * (1.0 + self.multiplier_percentage as f32 / 100.0) + self.addend as f32 + 0.5).floor() as i32
+        tools::tune(self.base, self.multiplier_percentage, self.addend)
     }
 }
 pub struct CharacterAttributes {
