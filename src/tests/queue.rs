93cb6756688acd6ed4d777e6cbfe598fb2197fad
minstrike520 <minstrike520@gmail.com>
2023-10-07 15:56:27 +0800

    .

--- src/tests/queue.rs
+++ src/tests/queue.rs
@@ -2,6 +2,7 @@ use crate::game::{ queue, chars };
 
 pub fn test_all() {
     queue_sort_by_id_test();
+    queue_character_partial_eq_test();
 }
 
 fn queue_sort_by_id_test() {
@@ -15,5 +16,18 @@ fn queue_sort_by_id_test() {
         chars::Character::new(1, 10, 10, 100),
         chars::Character::new(2, 10, 10, 100),
         chars::Character::new(3, 10, 10, 100),
+    ])
+}
+
+fn queue_character_partial_eq_test() {
+    let q1 = queue::CharacterQueue( vec![
+        chars::Character::new(1, 154, 10, 100),
+        chars::Character::new(3, 1024, 50, 4442),
+        chars::Character::new(2, 25, 10, 1207),
+    ]);
+    assert_eq!(q1.0, vec![
+        chars::Character::new(1, 10, 10, 52),
+        chars::Character::new(3, 150, 990, 205),
+        chars::Character::new(2, 10, 10, 86),
     ])
 }
