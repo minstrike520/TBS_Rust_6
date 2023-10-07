e367ff6e0c9b0c519827b080bf0b995864819fc7
minstrike520 <minstrike520@gmail.com>
2023-10-07 09:46:09 +0800

    (skills) chore: use tune in skills calculate

--- src/game/skills.rs
+++ src/game/skills.rs
@@ -1,7 +1,15 @@
-use crate::game::chars;
+use crate::game::{
+    chars, tools
+};
+
 
 pub fn normal_attack (caster: &chars::Character, target: &mut chars::Character){
     // target.notice_event(Event::ValueEvent(ValueEvent::Attacked));
-    let val: i32 = (caster.attributes.atk.get() as f32 * 0.5 + 0.5).floor() as i32;
+    let val: i32 = tools::tune(caster.attributes.atk.get(), -50, 0);
     target.hp -= val;
 }
+/*
+pub fn instant_regen (caster: &chars::Character) {
+    let val: i32 = 
+}
+*/
