acb9c67af98e8ff4a8731c10750b3fbd47b2d86e
minstrike520 <minstrike520@gmail.com>
2023-10-07 11:58:53 +0800

    (tools) style: renamed multiplier_percentage -> to _addition_percentage_

--- src/game/tools.rs
+++ src/game/tools.rs
@@ -1,3 +1,3 @@
-pub fn tune(base: u32, multiplier_percentage: i32, addend: i32) -> u32 {
-    (base as f32 * (1.0 + multiplier_percentage as f32 / 100.0) + addend as f32 + 0.5).floor() as u32
+pub fn tune(base: u32, addition_percentage: i32, addend: i32) -> u32 {
+    (base as f32 * (1.0 + addition_percentage as f32 / 100.0) + addend as f32 + 0.5).floor() as u32
 }
