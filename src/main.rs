3317b58e660de58541edb15adf22c406e4ea60a7
minstrike520 <minstrike520@gmail.com>
2023-10-07 08:37:40 +0800

    Restart #1

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::collections::HashMap;
+
+mod game;
+
+// HashMap<&str, Vec<T: Tuner> >
+
+/*
+        HashMap::from([
+            ("atk", vec![ TunerFlat(-20) ]),
+            ("spd", vec![ TunerPerc(30), TunerFlat(5) ])
+        ]) 
+*/
+
+
+fn main() {
+
+    
+}
