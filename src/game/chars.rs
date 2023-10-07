d687860cff91b044c972a30f30ebf64362a09ba0
minstrike520 <minstrike520@gmail.com>
2023-10-07 09:26:58 +0800

    char basics and attributes

---
+++ src/game/chars.rs
@@ -0,0 +1,42 @@
+pub struct Attribute {
+    pub base: i32,
+    multiplier_percentage: f32,
+    addend: i32,
+}
+impl Attribute {
+    fn new(base: i32) -> Self {
+        Self { base, multiplier_percentage: 0.0, addend: 0 }
+    }
+    pub fn get(&self) -> i32 {
+        (self.base as f32 * (1.0 + self.multiplier_percentage) + self.addend as f32 + 0.5).floor() as i32
+    }
+}
+pub struct CharacterAttributes {
+    pub atk: Attribute,
+    pub spd: Attribute,
+    pub mhp: Attribute,
+}
+impl CharacterAttributes {
+    fn new(i_atk: i32, i_spd: i32, i_mhp: i32) -> Self {
+        let atk = Attribute::new(i_atk);
+        let spd = Attribute::new(i_spd);
+        let mhp = Attribute::new(i_mhp);
+        Self { atk, spd, mhp }
+    }
+}
+
+pub struct Character {
+    pub id: i32,
+    pub attributes: CharacterAttributes,
+    pub hp: i32,
+}
+
+impl Character {
+    pub fn new(id: i32, i_atk: i32, i_spd: i32, i_mhp: i32) -> Self {
+        Self {
+            id,
+            attributes: CharacterAttributes::new ( i_atk, i_spd, i_mhp ),
+            hp: i_mhp
+        }
+    }
+}
